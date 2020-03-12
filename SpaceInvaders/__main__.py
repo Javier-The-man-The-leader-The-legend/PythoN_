58373ab004d5d7a671ce2ecd832c89f256e97ddd
Javier <destructor86500@gmail.com>
2020-03-12 20:10:08 +0100

    Actualized .gitignore, and game work in progress

--- SpaceInvaders/__main__.py
+++ SpaceInvaders/__main__.py
@@ -5,13 +5,31 @@ from player import Player
 class SpaceInvaders(arc.Window):
     def __init__(self):
         super().__init__(800, 600, "Space Invaders", fullscreen=True)
-        self.player = Player()
+        self.player = Player(self.width / 2, self.height * 0.04)
+    
     def on_key_press(self, symbol, modifiers):
         if symbol == arc.key.ESCAPE:
             arc.close_window()
+        if symbol == arc.key.A:
+            self.player.move_left()
+        if symbol == arc.key.D:
+            self.player.move_right()
+
+    def on_key_release(self, symbol, modifiers):
+        if symbol == arc.key.A:
+            self.player.stop()
+        if symbol == arc.key.D:
+            self.player.stop()
+    
     def on_draw(self):
         arc.start_render()
-        
+        self.player.draw()
+
+    def on_update(self, time):
+        self.player.update()
 if __name__ == "__main__":
+    from sys import argv
+    from os import chdir
+    chdir(argv[0].rpartition("/")[0])
     SpaceInvaders()
     arc.run()
--- SpaceInvaders/player.py
+++ SpaceInvaders/player.py
@@ -1,4 +1,13 @@
 from arcade import Sprite
 class Player(Sprite):
-    def __init__(self):
-        super().__init__("SpaceInvaders/spaceship.png", image_x=0, image_y=0, image_width=72, image_height=72)
+    def __init__(self, x, y):
+        super().__init__("spaceship.png", image_x=0, image_y=0, image_width=72, image_height=72)
+        self.center_x = x
+        self.center_y = y
+
+    def move_left(self):
+        self.change_x = -5
+    def move_right(self):
+        self.change_x = 5
+    def stop(self):
+        self. change_x = 0
